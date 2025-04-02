ca3c39465af96ea4e80cf782063cb6946083e711
TIBIO117 <153857496+TIBIO117@users.noreply.github.com>
2025-04-02 08:51:04 +0000

    gg

--- esercizio 4/operazioni.java
+++ esercizio 4/operazioni.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calcolatrice {
+public class operazioni {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
